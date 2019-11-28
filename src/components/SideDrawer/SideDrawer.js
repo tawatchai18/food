8938e19774e231dd02b70d5da6353676beb14e74
tawat chai <tawat1838@gmail.com>
2019-11-28 10:09:25 +0700

    แก้ url tab Dashboard / How to use

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -10,8 +10,8 @@ const sideDrawer = props => {
         <nav className={drawerClasses}>
             <ul>
                 <li ><a href="https://ffc.in.th/#What">Home</a></li>
-                <li ><a href="https://ffc.in.th/#tour">Dashboard</a></li>
-                <li ><a href="https://ffc.in.th/FAQ-ffc.html">How to use</a></li>
+                <li ><a href="https://ffc.in.th/dashboard/index.html#/dashboard/analytics">Dashboard</a></li>
+                <li ><a href="https://ffc.in.th/Use/">How to use</a></li>
                 <li ><a href="https://download.ffc.in.th">Download</a></li>
                 <li><a href="https://www.ffc.in.th/blog/">Blog</a></li>
                 <li ><a href="https://www.ffc.in.th/contact/">Contact</a></li>
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -17,8 +17,8 @@ const toolbar = props => (
             <div className="toolbar_navigation-items" style={{ fontSize: 16, marginRight: 160 }}>
                 <ul>
                     <li ><a href="https://ffc.in.th/#What">Home</a></li>
-                    <li ><a href="https://ffc.in.th/#tour">Dashboard</a></li>
-                    <li ><a href="https://ffc.in.th/FAQ-ffc.html">How to use</a></li>
+                    <li ><a href="https://ffc.in.th/dashboard/index.html#/dashboard/analytics">Dashboard</a></li>
+                    <li ><a href="https://ffc.in.th/Use/">How to use</a></li>
                     <li ><a href="https://download.ffc.in.th">Download</a></li>
                     <li><a href="https://www.ffc.in.th/blog/">Blog</a></li>
                     <li ><a href="https://www.ffc.in.th/contact/">Contact</a></li>
